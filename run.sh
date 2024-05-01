50a380b84dccc5e722774fcda9a79800f058a595
Viet Tran <viettq@amazon.com>
2024-05-01 18:23:51 +0700

    update k8s & eks training content

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "🚀 Start commiting code"
-# dt=`date '+%d/%m/%Y_%H:%M:%S'`
-# echo $dt > /Users/viettq/work/dev/demo/eks/tutorials/logs.txt
-git add .
-git commit -m "update k8s & eks training content"
-git push origin main
-echo "✅ Completed!"
